5ab44f3caa434669f27423e0cf5c7d7a734624b0
Madara Uchiha <me@madara.ninja>
2017-11-01 21:16:21 +0200

    Working build with postgres integration

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
+import { Client } from 'pg';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const client = new Client({connectionString: process.env.DATABASE_URL});
+    await client.connect();
+
+    const pgRes = await client.query('SELECT $1::text as message', ['Hello world!'])
+    await client.end()
+
     res.writeHead(200);
-    res.end('Hello World!!!');
+    res.end(pgRes.rows[0].message);
 })
 
 app.listen(8080);
